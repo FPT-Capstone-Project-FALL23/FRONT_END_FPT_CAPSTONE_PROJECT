93ae0c29a97827ec4f64cd12a87481fa2a17d822
ThuyPTT <thanhthuymkht@gmail.com>
2023-10-21 10:22:26 +0700

    Fix error of deleted code.

--- tikseat/src/Pages/Organizers/HomePageOrganizer.jsx
+++ tikseat/src/Pages/Organizers/HomePageOrganizer.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Grid,
+  Button,
+  List,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Outlet } from "react-router-dom";
+import MainListItems from "../../Components/Organizers/listItem";
+import "../../Assets/CSS/Common/LayoutSign.css";
 
 function HomePageOrganizer() {
   return (
     <>
-        <h1>HomePageOrganizer</h1>
+      <Grid>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">Dashboard</Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid
+          style={{
+            height: "100vh",
+            width: "100%",
+            borderRight: "2px solid #ccc",
+            display: "flex",
+          }}
+        >
+          <Grid
+            style={{
+              position: "fixed",
+              top: "65px", // Đặt khoảng cách từ đỉnh của AppBar
+              height: "100%",
+              width: "20%",
+              backgroundColor: "pink",
+              overflowY: "auto", // Thêm thanh cuộn nếu cần thiết
+            }}
+          >
+            <List component="nav">
+              <MainListItems />
+            </List>
+          </Grid>
+          <Grid
+            style={{
+              position: "absolute",
+              left: "20%",
+              top: "65px", // Đặt khoảng cách từ đỉnh của AppBar
+              padding: "20px 50px 0px 50px",
+              width: "80%",
+            }}
+          >
+            <Outlet />
+          </Grid>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default HomePageOrganizer
+export default HomePageOrganizer;
--- tikseat/src/Pages/RoutePage.jsx
+++ tikseat/src/Pages/RoutePage.jsx
@@ -15,13 +15,20 @@ const SendEmail = lazy(() => import("./Common/SendEmail"));
 const ProfileOrganizers = lazy(() => import("./Organizers/ProfileOrganizers"));
 const ProfileClient = lazy(() => import("./Client/ProfileClient"));
 const BookTickets = lazy(() => import("./Common/BookTickets"));
-
 // Trang tam thoi
 const HomePageClient = lazy(() => import("../Pages/Client/HomePageClient"));
 const HomePageAdmin = lazy(() => import("../Pages/Admin/HomePageAdmin"));
 const HomePageOrganizer = lazy(() =>
   import("../Pages/Organizers/HomePageOrganizer")
 );
+
+const DefaultDashboard = lazy(() =>
+  import("../Pages/Organizers/DefaultDashboard")
+);
+const EventHistory = lazy(() => import("../Pages/Organizers/EventHistory"));
+const NewEvent = lazy(() => import("../Pages/Organizers/NewEvent"));
+const SecondPage = lazy(() => import("../Pages/Organizers/SecondPage"));
+const CreateTicket = lazy(() => import("../Pages/Organizers/CreateTicket"));
 //
 
 function RoutePage() {
@@ -33,15 +40,23 @@ function RoutePage() {
         <Route path="/homepageClient" element={<HomePageClient />} />
         <Route path="/login" element={<LoginPage />} />
 
-        <Route element={<SignUp />}>
+        <Route element={<HomePageOrganizer />}>
           <Route path="/profileOrganizers" element={<ProfileOrganizers />} />
-          <Route path="/profileClient" element={<ProfileClient />} />
-        </Route>
-
-        <Route element={<LayoutSign />}>
           <Route path="/add-payment" element={<AddPaymentMethod />}></Route>
+          <Route path="/dashboard" element={<DefaultDashboard />}></Route>
+          <Route path="/event-history" element={<EventHistory />}></Route>
+          <Route path="/create-event" element={<NewEvent />}></Route>
+          <Route path="/second" element={<SecondPage />}></Route>
+          <Route path="/create-ticket" element={<CreateTicket />}></Route>
         </Route>
-
+        <Route path="/book-tickets" element={<BookTickets />}></Route>
+        <Route element={<SignUp />}>
+          <Route
+            path="/createProfileOrganizers"
+            element={<ProfileOrganizers />}
+          />
+          <Route path="/createProfileClient" element={<ProfileClient />} />
+        </Route>
         <Route
           element={
             <LayoutSign itemLeft={6} itemRight={6} direction="row-reverse" />
@@ -55,7 +70,6 @@ function RoutePage() {
           <Route path="/send-email" element={<SendEmail />}></Route>
         </Route>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/book-ticket" element={<BookTickets />}></Route>
       </Routes>
     </Suspense>
   );
