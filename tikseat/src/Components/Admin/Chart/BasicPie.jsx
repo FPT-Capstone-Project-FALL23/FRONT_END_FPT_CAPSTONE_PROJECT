35577ba1d320e96c4ab6b5a6da6ce6ff1b39bf9d
ChienNguyen <chiennguyen18102001c@gmail.com>
2023-12-22 02:47:27 +0700

    fix bug chart

--- tikseat/src/Components/Admin/Chart/BasicPie.jsx
+++ tikseat/src/Components/Admin/Chart/BasicPie.jsx
@@ -13,12 +13,7 @@ export default function BasicPie({
           data: [
             { id: 0, value: amountReceived, label: "The amount received" },
             { id: 1, value: amountRefund, label: "The amount of refund" },
-            {
-              id: 2,
-              value: 50400,
-              label: `The amount paid to \n the organization`,
-            },
-            { id: 3, value: adminRevenue, label: "Admin's profit amount" },
+            { id: 2, value: adminRevenue, label: "Admin's profit amount" },
           ],
         },
       ]}
